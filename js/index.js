48fdd8593b4b66a84f7121c1cfb3e840ca879342
stilldrewet <spalvej15@gmail.com>
2021-10-27 15:00:35 +0300

    test2

--- js/index.js
+++ js/index.js
@@ -4,13 +4,14 @@ let form=document.querySelector("#get_mail")
 let alertM=document.querySelector(".alert_tos");
 submitBtn.onclick=function(){
     if (checkBox.checked == true) {
+        alertM.style.display = 'none';
         form.setAttribute('action','/includes/action.php');
     }
     else{
         form.removeAttribute('action');
-    alertM.style.display = 'flex';
-    alertM.style.right = "10px";
-    return false;
+        alertM.style.display = 'flex';
+        alertM.style.right = "10px";
+        return false;
     }
 checkBox.onclick=function(){
     if (checkBox.checked == true) {
